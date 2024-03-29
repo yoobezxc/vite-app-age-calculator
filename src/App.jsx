bc46364907e5007c7380376d62c68ac9f0364119
yoobezxc <yoobedev@gmail.com>
2024-03-29 19:50:46 +0700

    add: setup file

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-black p-40 text-5xl font-black text-blue-600">
+        Hello World
+      </div>
+    </>
+  );
+}
+
+export default App;
